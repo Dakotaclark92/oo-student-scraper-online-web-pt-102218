c561fdd4cac9c5fed5c5637ef93ac3b2ecf6c9c0
Dakota Clark <bannaray95@gmail.com>
2019-01-06 04:59:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
- def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
     index_page.css("div.roster-cards-container").each do |card|
@@ -14,7 +14,6 @@ class Scraper
       end
     end
     students
-
   end
 
   def self.scrape_profile_page(profile_url)
@@ -33,4 +32,3 @@ class Scraper
   end
 
 end
-
