658b1b1922d281e3b0d5401f753bcc8fc21b8c41
Dakota Clark <bannaray95@gmail.com>
2019-01-06 05:13:26 +0000

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,37 +4,35 @@ require 'nokogiri'
 require 'colorize'
 
 class CommandLineInterface
-  BASE_PATH = "./fixtures/student-site/"
+  INDEX_URL = "http://learn-co-curriculum.github.io/student-scrape-site/"
+  BASE_PROFILE_URL = "http://learn-co-curriculum.github.io/student-scrape-site/profile.html"
+  BASE_URL = "http://students.learn.co"
 
   def run
+    self.make_students
+    self.add_attributes_to_students
+    self.display_students
     make_students
     add_attributes_to_students
     display_students
   end
 
   def make_students
-    students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
+    students_array = Scraper.scrape_index_page(INDEX_URL)
+    students_array = Scraper.scrape_index_page(BASE_URL)
     Student.create_from_collection(students_array)
   end
 
   def add_attributes_to_students
     Student.all.each do |student|
-      attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
+      profile_url = BASE_PROFILE_URL
+      # + "#{student.name}"
+      attributes = Scraper.scrape_profile_page(profile_url)
+      attributes = Scraper.scrape_profile_page(student.profile_url)
       student.add_student_attributes(attributes)
     end
   end
-
-  def display_students
-    Student.all.each do |student|
-      puts "#{student.name.upcase}".colorize(:blue)
-      puts "  location:".colorize(:light_blue) + " #{student.location}"
-      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
-      puts "  bio:".colorize(:light_blue) + " #{student.bio}"
-      puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
-      puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
-      puts "  github:".colorize(:light_blue) + " #{student.github}"
-      puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-      puts "----------------------".colorize(:green)
+@@ -40,4 +37,4 @@ def display_students
     end
   end
 
