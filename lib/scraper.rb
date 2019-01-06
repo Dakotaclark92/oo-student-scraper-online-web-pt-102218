b8b9464b6b927dbeabdff3e767e4d85891ea5f98
Dakota Clark <bannaray95@gmail.com>
2019-01-06 05:06:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,43 +1,34 @@
-require_relative "../lib/scraper.rb"
-require_relative "../lib/student.rb"
-require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
-class CommandLineInteface
-  INDEX_URL = "http://learn-co-curriculum.github.io/student-scrape-site/"
-  BASE_PROFILE_URL = "http://learn-co-curriculum.github.io/student-scrape-site/profile.html"
+class Scraper
 
-  def run
-    self.make_students
-    self.add_attributes_to_students
-    self.display_students
-  end
-
-  def make_students
-    students_array = Scraper.scrape_index_page(INDEX_URL)
-    Student.create_from_collection(students_array)
-  end
-
-  def add_attributes_to_students
-    Student.all.each do |student|
-      profile_url = BASE_PROFILE_URL
-      # + "#{student.name}"
-      attributes = Scraper.scrape_profile_page(profile_url)
-      student.add_student_attributes(attributes)
+  def self.scrape_index_page(index_url)
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".card-text-container").each do |container|
+        student_name = container.css(".student-name").text
+        student_location = container.css(".student-location").text
+        students << {name: student_name, location: student_location}
+      end
     end
+    students
   end
 
-  def display_students
-    Student.all.each do |student|
-      puts "#{student.name.upcase}:"
-      puts "  #{student.location}"
-      puts "  #{student.profile_quote}"
-      puts "  #{student.bio}"
-      puts "  #{student.twitter}"
-      puts "  #{student.linkedin}"
-      puts "  #{student.github}"
-      puts "  #{student.blog}"
-      puts "----------------------"
-    end
+  def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
+    student = {}
+
+    student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
+    student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value
+    student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value
+    student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value
+
+    student[:profile_quote] = profile_page.css(".profile-quote").text
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
+
+    student
   end
 
-end 
+end
